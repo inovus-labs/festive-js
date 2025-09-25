87c710e3eb3381d702716d3bbbe2f1b05616543c
Arjun Krishna <arjun12345krishna@gmail.com>
2025-09-25 07:29:49 +0530

    feat : [package] - Auto Theme Reg. with Vite build, Example Themes (xmas)

--- package/entry.glob.js
+++ package/entry.glob.js
@@ -1,10 +1,11 @@
-import Festive, { registerTheme } from "./src/index.js";
+
+import Festive from "./src/index.js";
 
 const modules = import.meta.glob("./themes/**/theme.js", { eager: true });
 for (const path in modules) {
   const mod = modules[path];
   const theme = mod?.default;
-  if (theme?.key) registerTheme(theme);
+  if (theme?.key) Festive.registerTheme(theme);
 }
 
 export default Festive;
