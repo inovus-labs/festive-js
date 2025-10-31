c9381b7217e6fd624e47ba96542466af81eab65e
harshilp1234 <harshilp1234@gmail.com>
2025-10-31 10:34:37 +0000

    feat(theme) : Updated Indepedence day theme to add animated waving Japan flag variant

--- package/themes/independence-day/index.js
+++ package/themes/independence-day/index.js
@@ -7,10 +7,10 @@
  */
 
 import india from './india.js';
-
+import japan from './japan.js';
 // Add other flags here when available:
 // import usa from './usa.js';
-const flags = [india]; // e.g., const flags = [india, usa];
+const flags = [india,japan]; // e.g., const flags = [india, usa];
 
 const allTriggers = flags.flatMap(f => f.triggers || []);
 
