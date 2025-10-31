8881520eeeb9427d9cd2c3226f0604d44cfd0680
harshilp1234 <harshilp1234@gmail.com>
2025-10-31 13:15:53 +0000

    feat(theme) : Updated Indepedence day theme to add animated waving Bahrain flag variant

--- package/themes/independence-day/index.js
+++ package/themes/independence-day/index.js
@@ -7,10 +7,11 @@
  */
 
 import india from './india.js';
+import bahrain from './bahrain.js';
 
 // Add other flags here when available:
 // import usa from './usa.js';
-const flags = [india]; // e.g., const flags = [india, usa];
+const flags = [india,bahrain]; // e.g., const flags = [india, usa];
 
 const allTriggers = flags.flatMap(f => f.triggers || []);
 
